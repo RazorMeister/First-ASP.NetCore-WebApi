b0107cf2dcb5bafcd392adb0238cd43fc3466440
Tymoteusz Bartnik <battnik90@gmail.com>
2020-11-13 13:27:50 +0100

    First 2 get methods.

---
+++ WebApplication/Controllers/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"} 
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
---
+++ WebApplication/Models/RphClass.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.Models
+{
+    public enum RphClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
